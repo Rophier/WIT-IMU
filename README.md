d91f84f53f64160e7998470c4b766dd8a4c2763e
Kyuhyong <kyuhyong@gmail.com>
2022-07-18 03:30:57 +0900

    Added config, launch

--- README.md
+++ README.md
@@ -1,5 +1,62 @@
-WIT-IMU
-    This is a ROS driver package for WIT IMU devices.
-    Support Devices:
-        JY901
-        WT901C
+# WIT-IMU for ros2
+    
+This is a ROS 2 implementation of WIT IMU driver package.  
+Support Devices:
+- JY901
+- WT901C (tested)
+
+# Dependency
+
+Serial Communication Library from https://github.com/RoverRobotics-forks/serial-ros2 
+
+To install this library  
+
+```
+$ git clone https://github.com/RoverRobotics-forks/serial-ros2.git
+$ cd serial-ros2
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+$ make install
+```
+
+# Installation
+
+Clone this project into your ros2 workspace and build
+```
+$ cd to ros2_ws/src
+$ git clone https://github.com/kyuhyong/WIT-IMU.git -b foxy
+$ cd ..
+$ colcon build --symlink-install
+```
+This will install wit-imu-driver package.  
+Please don't forget to source the **install/setup.bash** file for the first time build.
+
+# Device setup
+
+It is required to properly setup the imu device using WIT's proprietary software from below link.  
+https://www.wit-motion.com/digital-inclinometer/witmotion-wt901c-ttl-9-axis-imu-sensor.html
+
+You will need to check these parameters
+- Baudrate: default is 9600
+- Output Hz: default is 10
+- Magnetometer output
+- Quaternion output: need to be set for TF 
+
+# Launch node
+
+wt901c.launch.py is provided to run wit-imu-driver_node with wt901c.yaml file in /config.  
+
+```
+$ ros2 launch wit-imu-driver wt901c.launch.py
+```
+wt901c.yaml file includes parameters you can modify per the device setup.  
+
+
+# Run node
+To just run the node, simply enter
+
+```
+$ ros2 run wit-imu-driver wit-imu-driver_node
+```
--- src/wit_imu_node.cpp
+++ src/wit_imu_node.cpp
@@ -4,7 +4,11 @@
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 #include "sensor_msgs/msg/magnetic_field.hpp"
-// #include <std_msgs/Empty.h>
+
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "nav_msgs/msg/odometry.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 
@@ -19,14 +23,13 @@ public:
     WitImuNode()
     : Node("wit_imu_node")
     {
-        this->port = "/dev/ttyUSB0";
-        this->imu_frame = "imu_link";
-        pub_imu = this->create_publisher<sensor_msgs::msg::Imu>("imu_data", 1000);
-        pub_mag = this->create_publisher<sensor_msgs::msg::MagneticField>("mag_data", 1000);
+        this->configure();
+        pub_imu = this->create_publisher<sensor_msgs::msg::Imu>(imu_topic, 1000);
+        pub_mag = this->create_publisher<sensor_msgs::msg::MagneticField>(mag_topic, 1000);
+        tf_broadcaster =std::make_unique<tf2_ros::TransformBroadcaster>(*this);
         imu = CJY901();
         try
         {
-            //设置串口属性，并打开串口
             ser.setPort(port);
             ser.setBaudrate(baudrate);
             serial::Timeout to = serial::Timeout::simpleTimeout(500);
@@ -38,7 +41,6 @@ public:
             RCLCPP_INFO(this->get_logger(), "Unable to open port ");
             return;
         }
-        //检测串口是否已经打开，并给出提示信息
         if (ser.isOpen())
         {
             RCLCPP_INFO(this->get_logger(), "Serial Port initialized");
@@ -50,7 +52,7 @@ public:
         // ser.flush();
         int size;
         timer_ = this->create_wall_timer(
-            50ms, std::bind(&WitImuNode::timer_callback, this));
+            timer_ms, std::bind(&WitImuNode::timer_callback, this));
     }
 
 private:
@@ -61,20 +63,21 @@ private:
         {
             //ROS_INFO_ONCE("Data received from serial port.");
             int num;
+            rclcpp::Time now = this->get_clock()->now();
             unsigned char read_buf[count];
             num = ser.read(read_buf, count);
             imu.FetchData((char *)read_buf, num);
-            // ROS_INFO("IMU Data : Quaternion{ x: %f  y: %f  z: %f  w: %f} ", imu.quat.x, imu.quat.y, imu.quat.z, imu.quat.w);
             sensor_msgs::msg::Imu imu_data;
 
-            imu_data.header.stamp = this->get_clock()->now();
+            imu_data.header.stamp = now;
             imu_data.header.frame_id = imu_frame;
 
             imu_data.linear_acceleration.x = imu.acc.x;
             imu_data.linear_acceleration.y = imu.acc.y;
             imu_data.linear_acceleration.z = imu.acc.z;
             imu_data.linear_acceleration_covariance = {1e-6, 0, 0, 0, 1e-6, 0, 0, 0, 1e-6};
-
+            //RCLCPP_INFO(this->get_logger(), "Accel: x=%f, y=%f, z=%f",
+            //   imu.acc.x, imu.acc.y, imu.acc.z);
             imu_data.angular_velocity.x = imu.gyro.x;
             imu_data.angular_velocity.y = imu.gyro.y;
             imu_data.angular_velocity.z = imu.gyro.z;
@@ -85,8 +88,20 @@ private:
             imu_data.orientation.z = imu.quat.z;
             imu_data.orientation.w = imu.quat.w;
             imu_data.orientation_covariance = {1e-6, 0, 0, 0, 1e-6, 0, 0, 0, 1e-6};
-
+            //RCLCPP_INFO(this->get_logger(), "Quaternion: x=%f, y=%f, z=%f, w=%f", 
+            //   imu.quat.x, imu.quat.y, imu.quat.z, imu.quat.w);
             pub_imu->publish(imu_data);
+            if(is_pub_tf) {
+                geometry_msgs::msg::TransformStamped t;
+                t.header.stamp = now;
+                t.header.frame_id = this->base_frame;
+                t.child_frame_id = this->imu_frame;
+                t.transform.rotation.x = imu.quat.x;
+                t.transform.rotation.y = imu.quat.y;
+                t.transform.rotation.z = imu.quat.z;
+                t.transform.rotation.w = imu.quat.w;
+                this->tf_broadcaster->sendTransform(t);
+            }
 
             if (is_pub_mag)
             {
@@ -104,16 +119,57 @@ private:
             }
         }
     }
+    void configure()
+    {
+        this->declare_parameter<std::string>("port.name",       "/dev/ttyUSB0");
+        this->declare_parameter<int>        ("port.baudrate",   9600);
+        this->declare_parameter<std::string>("imu.topic",       "imu_data");
+        this->declare_parameter<int>        ("imu.output_hz",   20);
+        this->declare_parameter<bool>       ("mag.enable",      false);
+        this->declare_parameter<std::string>("mag.topic",       "mag_data");
+        this->declare_parameter<bool>       ("tf.enable",       false);
+        this->declare_parameter<std::string>("tf.base_frame",   "base_link");
+        this->declare_parameter<std::string>("tf.imu_frame",    "imu_link");
+
+        this->get_parameter<std::string>    ("port.name",       port);
+        this->get_parameter<int>            ("port.baudrate",   baudrate);
+        this->get_parameter<int>            ("imu.output_hz",   output_hz);
+        this->get_parameter<std::string>    ("imu.topic",       imu_topic);
+        this->get_parameter<bool>           ("mag.enable",      is_pub_mag);
+        this->get_parameter<std::string>    ("mag.topic",       mag_topic);
+        this->get_parameter<bool>           ("tf.enable",       is_pub_tf);
+        this->get_parameter<std::string>    ("tf.base_frame",   base_frame);
+        this->get_parameter<std::string>    ("tf.imu_frame",    imu_frame);
+        RCLCPP_INFO(this->get_logger(), "Params:");
+        RCLCPP_INFO(this->get_logger(), "\tport.Name: %s",      port.c_str());
+        RCLCPP_INFO(this->get_logger(), "\tport.Baudrate: %d",  baudrate);
+        RCLCPP_INFO(this->get_logger(), "\tIMU.Ouput_Hz: %d",   output_hz);
+        timer_ms = std::chrono::milliseconds {static_cast<long int>(1000 / output_hz)};
+        RCLCPP_INFO(this->get_logger(), "\tMilliseconds: %d",   timer_ms.count());
+        RCLCPP_INFO(this->get_logger(), "\tIMU.Topic: %s",      imu_topic.c_str());
+        RCLCPP_INFO(this->get_logger(), "\tMAG.Enable: %d",     is_pub_mag);
+        RCLCPP_INFO(this->get_logger(), "\tMAG.Topic: %s",      mag_topic.c_str());
+        RCLCPP_INFO(this->get_logger(), "\tTF.Base_link: %s",   base_frame.c_str());
+        RCLCPP_INFO(this->get_logger(), "\tTF.Imu_link: %s",    imu_frame.c_str());
+
+    }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr pub_imu;
-    rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr pub_mag;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr              pub_imu;
+    rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr    pub_mag;
     CJY901 imu;
     std::string port;
     int baudrate;
     int pub_rate;
+    int output_hz;
     bool is_pub_mag;
-    serial::Serial ser; //声明串口对象
+    serial::Serial ser;
+    bool is_pub_tf;
+    std::chrono::milliseconds timer_ms;
+    std::string imu_topic;
+    std::string mag_topic;
     std::string imu_frame;
+    std::string base_frame;
+    std::unique_ptr<tf2_ros::TransformBroadcaster>              tf_broadcaster;
 };
 
 int main(int argc, char * argv[])
